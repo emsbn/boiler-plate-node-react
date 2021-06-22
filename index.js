9652a18eb36a49985f7f1cab4c503db55c27b674
emsbn <emsbn1@gmail.com>
2021-06-22 20:12:51 +0900

    dev/prod

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
-require("dotenv").config();
+
+const config = require("./config/key");
 
 const { User } = require("./models/User");
 
@@ -11,22 +12,20 @@ app.use(bodyParser.json()); // application/json
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    `mongodb+srv://nuleonge:${process.env.mongo_db_password}@boilerplate.nchqd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!!!"));
 
 app.post("/register", (req, res) => {
   const user = new User(req.body);
+
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
